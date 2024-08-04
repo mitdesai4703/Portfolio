cfbf8cc90c1838f2fc9c92c806f76035302b1b03
Mit Desai <mitdesai4703@gmail.com>
2024-08-04 16:45:56 +0530

    changes in computer.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,10 +2,10 @@ import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-import CanvasLoader from "../Loader"; 
+import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const { scene } = useGLTF("/desktop_pc/scene.gltf"); 
+  const { scene } = useGLTF("/desktop_pc/scene.gltf");
 
   return (
     <mesh>
@@ -16,14 +16,14 @@ const Computers = ({ isMobile }) => {
         penumbra={1}
         intensity={2}
         castShadow
-        shadow-mapSize={[1024, 1024]} 
+        shadow-mapSize={[1024, 1024]}
       />
       <pointLight intensity={1.5} position={[10, 10, 10]} />
       <ambientLight intensity={1} />
       <primitive
         object={scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.4 : 0.75} 
+        position={isMobile ? [0, -2, -1.5] : [0, -3.25, -1.5]} 
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -34,18 +34,15 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    setIsMobile(mediaQuery.matches);
-
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
+    const updateIsMobile = () => {
+      setIsMobile(window.innerWidth <= 768); 
     };
 
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    updateIsMobile(); 
+    window.addEventListener("resize", updateIsMobile);
 
     return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+      window.removeEventListener("resize", updateIsMobile);
     };
   }, []);
 
@@ -53,9 +50,9 @@ const ComputersCanvas = () => {
     <Canvas
       frameloop='demand'
       shadows
-      dpr={[1, 2]}
+      dpr={[1, 2]} 
       camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }} 
+      gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
